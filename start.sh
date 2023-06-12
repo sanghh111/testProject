3acfe150f53bc4e20aa6b711b6adcb58e2ba8eb3
sangnt <sangnt@seatechit.com.vn>
2023-06-12 17:48:20 +0700

    no mess

--- start.sh
+++ start.sh
@@ -1,22 +1,2 @@
-echo "Loading the latest version"
-FILE=/home/testProject/pid/testProject
-if [ -f "$FILE" ]; then
-    kill $(cat /home/testProject/pid/testProject) || {
-        Port=3000
-        pid=`ps ax | grep gunicorn | grep $Port | awk '{split($0,a," "); print a[1]}' | head -n 1`
-        if [ -z "$pid" ]; then
-            echo "Server is stopping"
-        else
-            echo "Server is stopping"
-            rm -f /home/oncheck/pid/oncheck
-            kill $pid
-        fi
-    }
-    echo "Server is restarting"
-else 
-    echo "Server is starting"
-fi
-
-eval "$(conda shell.bash hook)"
 source venv/bin/activate
 gunicorn --bind 127.0.0.1:3000 --workers 1 --timeout 300 setting.wsgi --daemon
